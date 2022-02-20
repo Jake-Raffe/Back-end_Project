28aea1089be3d16f734d732dccb25e1f6cf7db99
Jake Raffe <jakeraffe24@gmail.com>
2022-02-20 15:32:22 +0000

    Added Get, Post and update options to NHSController class

---
+++ src/main/java/com/bnta/NHSController.java
@@ -0,0 +1,41 @@
+package com.bnta;
+
+import com.bnta.patient.PatientService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class NHSController {
+
+    private PatientService patientService;
+    private AppointmentService appointmentService;
+
+    public NHSController (PatientService patientService, AppointmentService appointmentService) {
+        this.patientService = patientService;
+        this.appointmentService = appointmentService;
+    }
+
+// Patient options:
+    // Add patient
+    @PostMapping(path = "patients")
+    public void addPatient(@RequestBody Patient patient){
+        patientService.addNewPatient(patient);
+    }
+
+    // Get all patients
+    @GetMapping(path = "patients")
+    public List<Patient> getPatients(){
+        return patientService.getAllPatients();
+    }
+
+    // Get patient by ID
+    @GetMapping(path = "patients/{id}")
+    public List<Patient> getPatients(@PathVariable("id") Integer patientId){
+        return patientService.getAllPatients(patientId);
+    }
+
+    // Update patient details
+    @PutMapping("/patients/{id}")
+    public void updateCarById(@RequestBody Patient patient, @PathVariable("id") Integer id) {
+        patientService.updatePatient(id, patient);
+    }
+}
---
+++ src/main/java/com/bnta/patient/PatientService.java
@@ -0,0 +1,6 @@
+package com.bnta.patient;
+
+public class PatientService {
+
+    private PatientDAO patientDAO;
+}
