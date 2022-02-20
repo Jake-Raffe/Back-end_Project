3720b9711316221ae372e45a2c49aae725a5af46
Michael Atayero <michaelak1395@gmail.com>
2022-02-20 17:33:33 +0000

    Appointment package added

---
+++ src/main/java/com/bnta/Appointment/Appointment.java
@@ -0,0 +1,85 @@
+package com.bnta.Appointment;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Appointment {
+    private int appointmentId;
+    private int patientNhsId;
+    private int doctorId;
+    private String appointmentRoom;
+    private LocalDateTime appointmentTime;
+
+    public Appointment(int appointmentId, int patientNhsId, int doctorId, String appointmentRoom, LocalDateTime appointmentTime) {
+        this.appointmentId = appointmentId;
+        this.patientNhsId = patientNhsId;
+        this.doctorId = doctorId;
+        this.appointmentRoom = appointmentRoom;
+        this.appointmentTime = appointmentTime;
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public int getPatientNhsId() {
+        return patientNhsId;
+    }
+
+    public void setPatientNhsId(int patientNhsId) {
+        this.patientNhsId = patientNhsId;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getAppointmentRoom() {
+        return appointmentRoom;
+    }
+
+    public void setAppointmentRoom(String appointmentRoom) {
+        this.appointmentRoom = appointmentRoom;
+    }
+
+    public LocalDateTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(LocalDateTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "appointmentId=" + appointmentId +
+                ", patientNhsId=" + patientNhsId +
+                ", doctorId=" + doctorId +
+                ", appointmentRoom='" + appointmentRoom + '\'' +
+                ", appointmentTime=" + appointmentTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Appointment that = (Appointment) o;
+        return appointmentId == that.appointmentId && patientNhsId == that.patientNhsId && doctorId == that.doctorId && Objects.equals(appointmentRoom, that.appointmentRoom) && Objects.equals(appointmentTime, that.appointmentTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appointmentId, patientNhsId, doctorId, appointmentRoom, appointmentTime);
+    }
+}
