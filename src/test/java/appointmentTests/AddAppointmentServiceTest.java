4150aa59758313b0650d77303d66785493a3a72b
suraya <suraya.hasan@hotmail.com>
2022-02-21 17:32:44 +0000

    apptTest2

--- src/test/java/appointmentTests/AddAppointmentServiceTest.java
+++ src/test/java/appointmentTests/AddAppointmentServiceTest.java
@@ -1,11 +1,41 @@
 package appointmentTests;
+import com.bnta.Appointment.Appointment;
+import com.bnta.Appointment.AppointmentDAO;
+import com.bnta.Appointment.AppointmentService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.*;
 
 
 public class AddAppointmentServiceTest {
 
-    @Test
-    void sH() {
+    @Mock private AppointmentDAO appointmentDAO;
+    private AppointmentService underTest;
+
+    //addAppointment
+    @BeforeEach
+    void successfulAddAppointment(){
+        //Given
+        Appointment testAppointment = new Appointment(1,
+                2,
+                3, LocalDateTime.of(10, 1300, 10, 3);
+
+
+
+
+
+        //When
+        //Then
+
     }
+
+
+
+
+
+
 }
