9a2c1ad757fdd5646388507a9b4f35fb5e93066d
Hajr Delli <hajr_delli1@hotmail.co.uk>
2022-02-22 22:36:40 +0000

    simplified mockito

--- src/test/java/appointmentTests/AddAppointmentServiceTest.java
+++ src/test/java/appointmentTests/AddAppointmentServiceTest.java
@@ -16,12 +16,14 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 
 public class AddAppointmentServiceTest {
 
-    @Mock private AppointmentDAO appointmentDAO;
+    @Mock
+    private AppointmentDAO appointmentDAO;
     private AppointmentService underTest;
     @BeforeEach
     void setUp() {
@@ -39,22 +41,18 @@ public class AddAppointmentServiceTest {
                 LocalDate.of(2022,Month.JUNE, 12),
                 LocalTime.of(14, 23));
 
-
-        Mockito.when(appointmentDAO.bookAppointment(eq(testAppointment))).thenReturn(1);
-        Mockito.when(appointmentDAO.viewAppointment()).thenReturn(List.of(new Appointment(1,
+        given(appointmentDAO.bookAppointment(eq(testAppointment))).willReturn(1);
+        given(appointmentDAO.viewAppointment()).willReturn(List.of(new Appointment(1,
                 2,
                 3,
                 LocalDate.of(2022,Month.JUNE, 12),
                 LocalTime.of(14, 23))));
 
-
-
         //When
         int result = underTest.bookAppointment(testAppointment);
         ArgumentCaptor<Appointment> appointmentArgumentCaptor = ArgumentCaptor.forClass(Appointment.class);
-        Mockito.verify(appointmentDAO).bookAppointment(appointmentArgumentCaptor.capture());
+        verify(appointmentDAO).bookAppointment(appointmentArgumentCaptor.capture());
         Appointment expectedAppointment = appointmentArgumentCaptor.getValue();
-
 
         //Then
         assertThat(expectedAppointment).isEqualTo(testAppointment);
@@ -64,9 +62,22 @@ public class AddAppointmentServiceTest {
 
 //unsuccessful adding appointment
     //testing the illegal exception - error
-
+//
+//    //Given
+//    Food food = new Food(1, 1, "toast", MealType.BREAKFAST, "random", 50, 1, Day.MONDAY);
+//    Person personInDb = new Person(1, "marcy", 23, 157.0, 47.0, 2000);
+//    // we pass in person Id using food.getPerson_id (getter for Food Class - as personId is a property of it)
+//    given(personDao.getPersonById(food.getPerson_id())).willReturn(personInDb);
+//    given(foodDao.addFood(food)).willReturn(1);
+//
+//    //When
+//    Integer actual = underTest.addFoodEntry(food);
+//    //Then
+//    // Assert the integer is 1
+//    Integer expected = 1;
+//    assertThat(actual).isEqualTo(expected);
     @Test
-    void unsuccessfulAddAppointment(){
+    void shouldThrowExceptionIfAddAppointmentUnsuccessful(){
         //Given
         Appointment testAppointment1 = new Appointment(1,
                 2,
@@ -95,8 +106,9 @@ public class AddAppointmentServiceTest {
 
              }).hasMessage("Could not book new appointment");
 
-
     }
+
+
 
 
 
