9c9c0dd8d64e9816757aa4040c1f59d5af32274d
suraya <suraya.hasan@hotmail.com>
2022-02-23 17:31:33 +0000

    appointment updates testing

--- src/main/java/com/bnta/appointment/AppointmentService.java
+++ src/main/java/com/bnta/appointment/AppointmentService.java
@@ -45,7 +45,7 @@ public class AppointmentService {
 
     public Appointment selectAppointmentById(Integer id) {
         try {
-            Appointment output = appointmentDAO.selectAppointmentById(id);
+           Appointment output = appointmentDAO.selectAppointmentById(id);
             if (output == null) {
                 throw new AppointmentNotFoundException("Appointment not found");
             }
@@ -78,15 +78,18 @@ public class AppointmentService {
         }
     }
 
-        public void updateAppointment (Appointment update, Integer id){
+        public int updateAppointment (Integer id, Appointment update){
             try {
-                int output = appointmentDAO.updateAppointment(update, id);
+                int output = appointmentDAO.updateAppointment(id, update);
                 if (output != 1) {
                     throw new IllegalStateException("Could not update appointment.");
                 }
             } catch (EmptyResultDataAccessException e) {
                 throw new AppointmentNotFoundException("Appointment with id " + id + " not found");
             }
+
+            //  return appointmentDAO.deleteAppointmentById(id);
+            return appointmentDAO.updateAppointment(id,update);
         }
 
     public List<Appointment> getAppointmentByPatientBloodType(String bloodType) {
